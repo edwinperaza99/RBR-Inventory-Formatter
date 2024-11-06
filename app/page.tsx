e67a387c65a46ada6a30951ca13ab3cdbdb747f8
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-11-06 01:58:58 -0800

    add date picker and initials input

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,20 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/toggle-theme-button";
 import { Switch } from "@/components/ui/switch";
+import { Calendar } from "@/components/ui/calendar";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
 	const [file, setFile] = useState<File | null>(null);
+	const [date, setDate] = useState<Date>();
+	const [initials, setInitials] = useState<string>("");
 
 	// Initialize states to default values without accessing localStorage directly
 	const [removeAuthor, setRemoveAuthor] = useState(false);
@@ -29,6 +40,14 @@ export default function Home() {
 			setRemoveAvailability(
 				localStorage.getItem("removeAvailability") === "true"
 			);
+			// Load date (if it exists) and parse it
+			const storedDate = localStorage.getItem("endDate");
+			if (storedDate) {
+				setDate(new Date(JSON.parse(storedDate)));
+			}
+
+			// Load initials
+			setInitials(localStorage.getItem("initials") || "");
 		}
 	}, []);
 
@@ -58,6 +77,17 @@ export default function Home() {
 			localStorage.setItem("removeAvailability", removeAvailability.toString());
 	}, [removeAvailability]);
 
+	useEffect(() => {
+		if (date) {
+			localStorage.setItem("endDate", JSON.stringify(date.toISOString()));
+		}
+	}, [date]);
+
+	useEffect(() => {
+		if (typeof window !== "undefined")
+			localStorage.setItem("initials", initials);
+	}, [initials]);
+
 	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const uploadedFile = e.target.files?.[0];
 		if (uploadedFile) setFile(uploadedFile);
@@ -75,6 +105,13 @@ export default function Home() {
 		formData.append("removeISBN", removeISBN.toString());
 		formData.append("removeEdition", removeEdition.toString());
 		formData.append("removeAvailability", removeAvailability.toString());
+		formData.append("initials", initials);
+		formData.append("endDate", date?.toISOString() || "");
+		if (date) {
+			const formattedDate = format(date, "MM/dd/yyyy");
+			formData.append("endDate", formattedDate);
+			console.log(formattedDate);
+		}
 
 		const response = await fetch("/api/processFile", {
 			method: "POST",
@@ -93,10 +130,10 @@ export default function Home() {
 		<main className="container mx-auto flex flex-col justify-center items-center min-h-screen">
 			<ModeToggle />
 			<h1 className="text-center text-3xl sm:text-5xl my-5">
-				RBR Inventory Formatter
+				RBR List Formatter
 			</h1>
 			<form
-				className="mt-4 space-y-2 "
+				className="space-y-3"
 				onSubmit={(e) => {
 					e.preventDefault(); // Prevents default form submission
 					handleFileUpload(); // Calls your custom upload function
@@ -144,15 +181,48 @@ export default function Home() {
 						Delete &quot;Availability&quot; Column
 					</Label>
 				</div>
-				<Label htmlFor="file-upload" className="mt-4 block">
-					Upload Excel File:
-				</Label>
-				<Input
-					id="file-upload"
-					type="file"
-					accept=".xls"
-					onChange={handleFileChange}
-				/>
+				<div className="flex flex-col gap-1">
+					<Label>End Date:</Label>
+					<Popover>
+						<PopoverTrigger asChild>
+							<Button
+								variant={"outline"}
+								className={cn(
+									"w-[280px] justify-start text-left font-normal",
+									!date && "text-muted-foreground"
+								)}
+							>
+								<CalendarIcon className="mr-2 h-4 w-4" />
+								{date ? format(date, "PPP") : <span>Pick a date</span>}
+							</Button>
+						</PopoverTrigger>
+						<PopoverContent className="w-auto p-0">
+							<Calendar
+								mode="single"
+								selected={date}
+								onSelect={setDate}
+								initialFocus
+							/>
+						</PopoverContent>
+					</Popover>
+				</div>
+				<div className="flex flex-col gap-1">
+					<Label>Initials:</Label>
+					<Input
+						type="text"
+						placeholder="This field is optional"
+						className="px-4"
+					/>
+				</div>
+				<div className="flex flex-col gap-1">
+					<Label htmlFor="file-upload">Upload Excel File:</Label>
+					<Input
+						id="file-upload"
+						type="file"
+						accept=".xls"
+						onChange={handleFileChange}
+					/>
+				</div>
 				<div className="mt-4">
 					<Button type="submit" disabled={!file}>
 						Process File
